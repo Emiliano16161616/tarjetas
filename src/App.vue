a8abd68d28d745d1c0d40c9e7b2e8b5fbbc33f06
Emiliano16161616 <jemiliosp24@gmail.com>
2023-06-24 00:35:23 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { ref } from 'vue';
+import cardComponent from './components/cardComponent.vue';
+const users = ref([
+  {
+    name:"juan jose mejor amigo",
+    email:"kakoso@gmail.com"
+  },
+  {
+    name:"el gallo",
+    email:"gallazo@gmail.com"
+  }
+])
+let name, email;
+const añadir= function(){
+  const newUser={
+    name:name,
+    email:email
+  }
+  users.value.unshift(newUser)
+}
+const borrarTarjeta=function(index){
+  users.value.splice(index,1)
+}
+</script>
+
+<template>
+  <cardComponent v-for="(user,index) in users" :key="index" :name="user.name" :email="user.email" :index="index" @borrarTarjeta="borrarTarjeta" />
+  <div>
+    <label class="text-white "> ingrese su nombre</label>
+    <br>
+    <input v-model="name" type="text">
+    <br>
+    <label class="text-white"> ingrese su correo</label>
+    <br>
+    <input v-model="email" type="text">
+    <br>
+    <button class="p-1 mt-3 bg-white rounded-lg" @click="añadir">enviar</button>
+  </div>
+</template>
---
+++ src/components/cardComponent.vue
@@ -0,0 +1,46 @@
+<script setup>
+const props= defineProps({
+    name:{
+        type: String,
+        default: "no hay nombre"
+    },
+    email:{
+        type: String,
+        default: "no hay correo"
+    },
+    index:{
+        type:Number
+    }
+})
+const borrarTarjeta=function(index){
+    emit("borrarTarjeta",index)
+}
+const emit= defineEmits("borrarTarjeta")
+</script>
+
+<template>
+  <div class="flex bg-red-500 h-72 w-72">
+    <div>
+        <img src="https://i.pinimg.com/originals/24/f1/f5/24f1f54f147a70157d56180b5192f6df.jpg" class="w-24 h-40" alt="">
+        <p>
+            {{ props.name }}
+        </p>
+        <p>
+            {{props.email}}
+        </p>
+        
+    </div>
+    <div class="flex ">
+        <div>
+            <p class="bg-blue-600 p-[15px] rounded-lg" @click="borrarTarjeta(props.index)">
+                x 
+            </p>
+        </div>
+        <div>
+            <p class="bg-purple-600 p-[15px] ml-5 rounded-lg" >
+                add
+            </p>
+        </div>
+    </div>
+  </div>
+</template>
